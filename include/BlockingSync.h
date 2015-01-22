04aed927f82fe307b089cdee26513a0a7dfb01ba
Saman Barghi <saman.b@gmail.com>
2015-01-22 16:02:17 -0500

    Replaced tabs with spaces.

--- include/BlockingSync.h
+++ include/BlockingSync.h
@@ -15,158 +15,171 @@
 #include <iostream>
 
 class Mutex;
+template<typename Buffer> class MessageQueue;
 
 class BlockingQueue {
-	friend class uThread;
-	friend class Mutex;
-	friend class ConditionVariable;
-	friend class Semaphore;
-	EmbeddedList<uThread> queue;
+    friend class uThread;
+    friend class Mutex;
+    friend class ConditionVariable;
+    friend class Semaphore;
+    template<typename Buffer> friend class MessageQueue;
+    EmbeddedList<uThread> queue;
 
-	bool suspend(std::mutex& lock);
-	bool suspend(Mutex&);
+    bool suspend(std::mutex& lock);
+    bool suspend(Mutex&);
 //  bool suspend(mutex& lock, mword timeout);
 //
-	BlockingQueue(const BlockingQueue&) = delete;                  // no copy
-	const BlockingQueue& operator=(const BlockingQueue&) = delete; // no assignment
+    BlockingQueue(const BlockingQueue&) = delete;                  // no copy
+    const BlockingQueue& operator=(const BlockingQueue&) = delete; // no assignment
 public:
-	BlockingQueue() = default;
-	~BlockingQueue() { assert(empty());}
-	bool empty() const { return queue.empty(); }
+    BlockingQueue() = default;
+    ~BlockingQueue() {
+        assert(empty());
+    }
+    bool empty() const {
+        return queue.empty();
+    }
 
-	// Thread*& passed to support atomic setting of Mutex::owner
-	bool signal(std::mutex& lock, uThread*& owner);			//Used along with Mutex
-	bool signal(Mutex&);														//Used with ConditionVariable
-	void signalAll(Mutex&);														//Used with ConditionVariable
+    // Thread*& passed to support atomic setting of Mutex::owner
+    bool signal(std::mutex& lock);			//Used along with Mutex
+    bool signal(Mutex&);						//Used with ConditionVariable
+    void signalAll(Mutex&);						//Used with ConditionVariable
 };
 
 class Mutex {
-	friend class ConditionVariable;
-	friend class Semaphore;
-	friend class BlockingQueue;
+    friend class ConditionVariable;
+    friend class Semaphore;
+    friend class BlockingQueue;
 protected:
-	std::mutex lock;
+    std::mutex lock;
 //	std::unique_lock<std::mutex> lock; //Create a unique_lock but do not lock the mutex yet
-	BlockingQueue bq;
-	uThread* owner;
+    BlockingQueue bq;
+    uThread* owner;
 
-	template<bool OL> // OL: owner lock
-	bool internalAcquire(mword timeout) {
+    template<bool OL> // OL: owner lock
+    bool internalAcquire(mword timeout) {
 
-		lock.lock();
-		assert(OL || owner != kThread::currentKT->currentUT);					//Mutex cannot have recursive locking
-		if fastpath(owner != nullptr && (!OL || owner != kThread::currentKT->currentUT)) {
-			return bq.suspend(lock); // release lock, false: timeout
-		} else {
-			/*
-			 * End up here if:
-			 * 	- No one is holding the lock
-			 * 	- Someone is holding the lock and:
-			 * 		+ The type is OwnlerLock and the calling thread is holding the lock
-			 * Otherwise, block and wait for the lock
-			 */
-			owner = kThread::currentKT->currentUT;
-			lock.unlock();
-			return true;
-		}
-	}
+        lock.lock();
+        assert(OL || owner != kThread::currentKT->currentUT);//Mutex cannot have recursive locking
+        if fastpath(owner != nullptr && (!OL || owner != kThread::currentKT->currentUT)) {
+            return bq.suspend(lock); // release lock, false: timeout
+        } else {
+            /*
+             * End up here if:
+             * 	- No one is holding the lock
+             * 	- Someone is holding the lock and:
+             * 		+ The type is OwnlerLock and the calling thread is holding the lock
+             * Otherwise, block and wait for the lock
+             */
+            owner = kThread::currentKT->currentUT;
+            lock.unlock();
+            return true;
+        }
+    }
 
-	void internalRelease() {
-		// try baton-passing, if yes: 'signal' releases lock
-		if fastpath(bq.signal(lock, owner)) return;
-		owner = nullptr;
-		lock.unlock();
-	}
+    void internalRelease() {
+        // try baton-passing, if yes: 'signal' releases lock
+        if fastpath(bq.signal(lock)) return;
+        owner = nullptr;
+        lock.unlock();
+    }
 
 public:
-	Mutex() : owner(nullptr){}
+    Mutex() : owner(nullptr) {}
 
-	template<bool OL=false>
-	bool acquire() {return internalAcquire<OL>(0);}
+    template<bool OL=false>
+    bool acquire() {return internalAcquire<OL>(0);}
 
 //  template<bool OL=false>
 //  bool tryAcquire(mword t = 0) { return internalAcquire<true,OL>(t); }
 
-	void release() {
-		lock.lock();
-		assert(owner == kThread::currentKT->currentUT);							//Attempt to release lock by non-owner
-		internalRelease();
-	}
+    void release() {
+        lock.lock();
+        assert(owner == kThread::currentKT->currentUT);//Attempt to release lock by non-owner
+        internalRelease();
+    }
 };
 
 class OwnerLock: protected Mutex {
-	mword counter;
+    mword counter;
 public:
-	OwnerLock() : counter(0) {}
-	mword acquire() {
-		if (internalAcquire<true>(0))
-			return ++counter;
-		else
-			return 0;
-	}
+    OwnerLock() :
+            counter(0) {
+    }
+    mword acquire() {
+        if (internalAcquire<true>(0))
+            return ++counter;
+        else
+            return 0;
+    }
 //  mword tryAcquire(mword t = 0) {
 //    if (internalAcquire<true,true>(t)) return ++counter;
 //    else return 0;
 //  }
-	mword release() {
-		lock.lock();
-		assert(owner == kThread::currentKT->currentUT); //, "attempt to release lock by non-owner");
-		if (--counter == 0)
-			internalRelease();
-		else
-			lock.unlock();
-		return counter;
-	}
+    mword release() {
+        lock.lock();
+        assert(owner == kThread::currentKT->currentUT); //, "attempt to release lock by non-owner");
+        if (--counter == 0)
+            internalRelease();
+        else
+            lock.unlock();
+        return counter;
+    }
 };
 
 class ConditionVariable {
-  BlockingQueue bq;
+    BlockingQueue bq;
 
 public:
-  bool empty() const { return bq.empty(); }
-  void wait(Mutex& mutex) { bq.suspend(mutex); mutex.acquire();}
-  void signal(Mutex& mutex) { if slowpath(!bq.signal(mutex)) mutex.release(); }
-  void signalAll(Mutex& mutex) { bq.signalAll(mutex); }
+    bool empty() const {
+        return bq.empty();
+    }
+    void wait(Mutex& mutex) {
+        bq.suspend(mutex);
+        mutex.acquire();
+    }
+    void signal(Mutex& mutex) {
+        if slowpath(!bq.signal(mutex)) mutex.release();}
+    void signalAll(Mutex& mutex) {bq.signalAll(mutex);}
 };
 
-
 class Semaphore {
-	Mutex mutex;
-	BlockingQueue bq;
-	mword counter;
+    Mutex mutex;
+    BlockingQueue bq;
+    mword counter;
 
-	template<bool TO>
-	bool internalP(mword timeout) {
-		if fastpath(counter < 1) {
-			return bq.suspend(mutex); // release lock, false: timeout
-		} else {
-			counter -= 1;
-			mutex.release();
-			return true;
-		}
-	}
+    template<bool TO>
+    bool internalP(mword timeout) {
+        if fastpath(counter < 1) {
+            return bq.suspend(mutex); // release lock, false: timeout
+        } else {
+            counter -= 1;
+            mutex.release();
+            return true;
+        }
+    }
 
 public:
-	explicit Semaphore(mword c = 0) : counter(c) {}
-	bool empty() const {return bq.empty();}
+    explicit Semaphore(mword c = 0) : counter(c) {}
+    bool empty() const {return bq.empty();}
 
-	bool P() {
-		mutex.acquire();
-		return internalP<false>(0);
-	}
+    bool P() {
+        mutex.acquire();
+        return internalP<false>(0);
+    }
 
 //	bool tryP(mword t = 0, SpinLock* l = nullptr) {
 //		if (l) lock.acquire(*l); else lock.acquire();
 //		return internalP<true>(t);
 //	}
 
-	void V() {
-		mutex.acquire();
-		// try baton-passing, if yes: 'signal' releases lock
-		if fastpath(bq.signal(mutex)) return;
-		counter += 1;
-		mutex.release();
-	}
+    void V() {
+        mutex.acquire();
+        // try baton-passing, if yes: 'signal' releases lock
+        if fastpath(bq.signal(mutex)) return;
+        counter += 1;
+        mutex.release();
+    }
 };
 
 #endif /* BLOCKINGSYNC_H_ */
--- src/BlockingSync.cpp
+++ src/BlockingSync.cpp
@@ -10,50 +10,51 @@
 
 bool BlockingQueue::suspend(std::mutex& lock) {
 
-	kThread::currentKT->currentUT->suspend(this, lock);
-	//TODO: we do not have any cancellation yet, so this line will not be reached at all
-	return true;
+    kThread::currentKT->currentUT->suspend(this, lock);
+    //TODO: we do not have any cancellation yet, so this line will not be reached at all
+    return true;
 }
 
-bool BlockingQueue::suspend(Mutex& mutex){
-	kThread::currentKT->currentUT->suspend(this, mutex);
-	return true;
+bool BlockingQueue::suspend(Mutex& mutex) {
+    kThread::currentKT->currentUT->suspend(this, mutex);
+    return true;
 }
 
-bool BlockingQueue::signal(std::mutex& lock, uThread*& owner) {
-	//TODO: handle cancellation
-	if(queue.front() != queue.fence()){							//Fetch one thread and put it back to ready queue
-		owner = queue.front();					//FIFO?
-		queue.pop_front();
-		lock.unlock();
+bool BlockingQueue::signal(std::mutex& lock) {
+    uThread* owner = nullptr;
+    //TODO: handle cancellation
+    if (queue.front() != queue.fence()) {//Fetch one thread and put it back to ready queue
+        owner = queue.front();					//FIFO?
+        queue.pop_front();
+        lock.unlock();
 //		printAll();
-		owner->resume();
-		return true;
-	}
-	return false;
+        owner->resume();
+        return true;
+    }
+    return false;
 }
 
 bool BlockingQueue::signal(Mutex& mutex) {
-	//TODO: handle cancellation
-	uThread* owner = nullptr;
-	if(!queue.empty()){							//Fetch one thread and put it back to ready queue
-		owner = queue.front();					//FIFO?
-		queue.pop_front();
-		mutex.release();
-		owner->resume();
-		return true;
-	}
-	return false;
+    //TODO: handle cancellation
+    uThread* owner = nullptr;
+    if (!queue.empty()) {	//Fetch one thread and put it back to ready queue
+        owner = queue.front();					//FIFO?
+        queue.pop_front();
+        mutex.release();
+        owner->resume();
+        return true;
+    }
+    return false;
 }
 
 void BlockingQueue::signalAll(Mutex& mutex) {
 
-	uThread* ut = queue.front();
-	for(;;){
-		if slowpath(ut == queue.fence()) break;
-		queue.remove(ut);
-		ut->resume();							//Send ut back to the ready queue
-		ut = queue.front();
-	}
-	mutex.release();
+    uThread* ut = queue.front();
+    for (;;) {
+        if slowpath(ut == queue.fence()) break;
+        queue.remove(ut);
+        ut->resume();						//Send ut back to the ready queue
+        ut = queue.front();
+    }
+    mutex.release();
 }
