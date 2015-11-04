c91c38c758fa4993788d9e185114f668071ca7f1
Saman Barghi <saman.b@gmail.com>
2015-11-04 16:22:26 -0500

    C wrapper for most uThreads objects.

---
+++ include/cwrapper.h
@@ -0,0 +1,67 @@
+/*
+ * cwrapper.h
+ *
+ *  Created on: Nov 26, 2014
+ *      Author: Saman Barghi
+ */
+
+#ifndef CWRAPPER_H_
+#define CWRAPPER_H_
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/**************Cluster*************/
+struct WCluster;
+typedef struct WCluster WCluster;
+WCluster* cluster_create();
+void cluster_destruct(WCluster* cluster);
+/**********************************/
+
+/*************kThread**************/
+struct WkThread;
+typedef struct WkThread WkThread;
+WkThread* kThread_create(WCluster* cluster);
+void kThread_destroy(WkThread* kt);
+//if linux
+pthread_t kThread_get_current_pthread_id(); //return pthread_t for current running thread
+pthread_t kThread_get_pthread_id(WkThread* kt); //return pthread_t for the provided kThread
+//endif linux
+uint64_t kThread_count();
+/**********************************/
+
+/*************uThread**************/
+struct WuThread;
+typedef struct WuThread WuThread;
+WuThread* uThread_create(void *(*start_routine) (void *), void *arg);
+WuThread* uThread_create_with_cluster(WCluster* cluster, void *(*start_routine) (void *), void *arg);
+void uThread_destroy(WuThread* ut);
+void uThread_yield();
+/**********************************/
+
+/*************Connection************/
+struct WConnection;
+typedef struct WConnection WConnection;
+WConnection* connection_create(int fd);
+void connection_destory(WConnection* c);
+
+ssize_t connection_recvfrom(WConnection* con, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+ssize_t connection_sendmsg(WConnection* con, const struct msghdr *msg, int flags);
+
+ssize_t connection_read(WConnection* con, void *buf, size_t count);
+ssize_t connection_write(WConnection* con, const void *buf, size_t count);
+
+int connection_close(WConnection* con);
+void connection_poll_open(WConnection* con);
+void connection_poll_reset(WConnection* con);
+/**********************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CWRAPPER_H_ */
---
+++ src/cwrapper.cpp
@@ -0,0 +1,57 @@
+/*
+ * cwrapper.cpp
+ *
+ *  Created on: Dec 1, 2014
+ *      Author: Saman Barghi
+ */
+
+#include "global.h"
+#include "cwrapper.h"
+#include "uThread.h"
+#include "kThread.h"
+#include "Network.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+/**************Cluster*************/
+WCluster* cluster_create(){  return reinterpret_cast<WCluster*>( new Cluster( ) );}
+void cluster_destroy(WCluster* cluster){ delete reinterpret_cast<Cluster*>(cluster); }
+/**********************************/
+
+/*************kThread**************/
+WkThread* kThread_create(WCluster* cluster){  return reinterpret_cast<WkThread*>( new kThread( reinterpret_cast<Cluster*>(cluster) ) );}
+void WkThread_destroy(WkThread* kt){ delete reinterpret_cast<kThread*>(kt); }
+pthread_t kThread_get_current_pthread_id(){ return kThread::currentKT->getThreadNativeHandle(); }
+pthread_t kThread_get_pthread_id(WkThread* kt){ return (reinterpret_cast<kThread*>(kt))->getThreadNativeHandle(); }
+uint64_t kThread_count(){ return kThread::totalNumberofKTs.load(); }
+/**********************************/
+
+
+/*************uThread**************/
+WuThread* uThread_create(void *(*start_routine) (void *), void *arg){ return reinterpret_cast<WuThread*>( uThread::create((funcvoid1_t)start_routine, arg)); }
+WuThread* uThread_create_with_cluster(WCluster* cluster, void *(*start_routine) (void *), void *arg){
+    return reinterpret_cast<WuThread*>( uThread::create( (funcvoid1_t)start_routine, arg, reinterpret_cast<Cluster*>(cluster) ) );
+}
+void uThread_destroy(WuThread* ut){ delete reinterpret_cast<uThread*>(ut); }
+void uThread_yield(){ kThread::currentKT->currentUT->yield(); }
+/**********************************/
+
+
+/*************Connection**************/
+WConnection* connection_create(int fd){ return reinterpret_cast<WConnection*>( new Connection(fd)); }
+void connection_destroy(WConnection* con){ delete reinterpret_cast<Connection*>(con); }
+ssize_t connection_recvfrom(WConnection* con, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen){
+    return reinterpret_cast<Connection*>(con)->recvfrom(buf, len, flags, src_addr, addrlen);
+}
+ssize_t connection_sendmsg(WConnection* con, const struct msghdr *msg, int flags){ return reinterpret_cast<Connection*>(con)->sendmsg(msg, flags);}
+
+ssize_t connection_read(WConnection* con, void *buf, size_t count){ return reinterpret_cast<Connection*>(con)->read(buf, count);}
+ssize_t connection_write(WConnection* con, const void *buf, size_t count){ return reinterpret_cast<Connection*>(con)->write(buf, count);}
+int connection_close(WConnection* con){ return reinterpret_cast<Connection*>(con)->close();}
+void connection_poll_open(WConnection* con){ return reinterpret_cast<Connection*>(con)->pollOpen();}
+void connection_poll_reset(WConnection* con){ return reinterpret_cast<Connection*>(con)->pollReset();}
+/**********************************/
+#ifdef __cplusplus
+}
+#endif
