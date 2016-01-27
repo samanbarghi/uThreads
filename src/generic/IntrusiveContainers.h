11512b5f5895674adb5d2024dcd91ee9b8a8591d
Saman Barghi <saman.b@gmail.com>
2016-01-27 12:12:02 -0500

    Updated copyright headers

--- src/generic/IntrusiveContainers.h
+++ src/generic/IntrusiveContainers.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-    Copyright � 2012-2015 Martin Karsten
+    Copyright © 2012-2015 Martin Karsten
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/io/Network.cpp
+++ src/io/Network.cpp
@@ -1,9 +1,20 @@
-/*
- * Network.cpp
+/*******************************************************************************
+ *     Copyright © 2015, 2016 Saman Barghi
  *
- *  Created on: Oct 21, 2015
- *      Author: Saman Barghi
- */
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
 #include "Network.h"
 #include "IOHandler.h"
 #include <unistd.h>
