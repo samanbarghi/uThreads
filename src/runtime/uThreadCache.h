d0fd1b57ecf99e6e5ffd96f3b8b65da8584a71b2
Saman Barghi <saman.b@gmail.com>
2016-01-28 12:12:41 -0500

    Updated the documentation for doxygen

--- src/runtime/uThreadCache.h
+++ src/runtime/uThreadCache.h
@@ -22,10 +22,19 @@
 #include "generic/IntrusiveContainers.h"
 #include "uThread.h"
 
-//caching interface for uThreads
+/**
+ * @class uThreadCache
+ * @brief Data structure to cache uThreads
+ *
+ * uThreadCache is a linked list of uThreads using and intrusive container
+ * to cache all terminated uThreads. Instead of destroying the memory allocated
+ * for the stack, simply reset the stack pointer and push it to the cache.
+ */
 class uThreadCache {
 
-    std::mutex mtx;             //global mutex for protecting the underlying data structure
+private:
+    //global mutex for protecting the underlying data structure
+    std::mutex mtx;
     IntrusiveList<uThread> stack;
     size_t count;
     size_t size;
@@ -34,17 +43,33 @@ public:
     uThreadCache(size_t size = defaultuThreadCacheSize) : count(0), size(size){};
     ~uThreadCache(){}
 
+    /**
+     * @brief adds a uThread to the cache
+     * @param ut pointer to a uThread
+     * @return size of the cache if push was successful or -1 if not
+     *
+     * This function tries to push a uThread into the cache structure. If
+     * the cache is full or the mutex cannot be acquired immediately the
+     * operation has failed and the function returns -1. Otherwise, it adds the
+     * uThread to the list and return the size of the cache.
+     */
     ssize_t push(uThread* ut){
         std::unique_lock<std::mutex> mlock(mtx, std::try_to_lock);
-        if(!mlock.owns_lock() || count == size) return -1;                      //do not block just to grab this lock
+        //do not block just to grab this lock
+        if(!mlock.owns_lock() || count == size) return -1;
         ut->reset();
         stack.push_back(*ut);
         return ++count;
     }
 
+    /**
+     * @brief pop a uThread from the list in FIFO order and return it
+     * @return nullptr on failure, or a pointer to a uThread on success
+     */
     uThread* pop(){
         std::unique_lock<std::mutex> mlock(mtx, std::try_to_lock);
-        if(!mlock.owns_lock() || count == 0) return nullptr;                    //do not block just to grab this lock
+        //do not block just to grab this lock
+        if(!mlock.owns_lock() || count == 0) return nullptr;
         uThread* ut = stack.back();
         stack.pop_back();
         count--;
