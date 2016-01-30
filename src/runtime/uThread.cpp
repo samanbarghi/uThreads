68e2c0883039201697a4be0b02a2177578d5b8a4
Saman Barghi <saman.b@gmail.com>
2016-01-30 03:53:29 -0500

    use munmap instead of free for uThread stack

--- src/runtime/uThread.cpp
+++ src/runtime/uThread.cpp
@@ -46,7 +46,7 @@ void uThread::destory(bool force = false) {
     //check whether we should cache it or not
     totalNumberofUTs--;
     if (slowpath(force) || (utCache.push(this) < 0)) {
-        free((ptr_t) (stackBottom));       //Free the allocated memory for stack
+        munmap((ptr_t) (stackBottom), stackSize);       //Free the allocated memory for stack
     }
 }
 
