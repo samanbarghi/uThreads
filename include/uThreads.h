c2bf87d4ab918c97ced8d85b91b48547c8450afb
Saman Barghi <saman.b@gmail.com>
2016-02-07 02:21:21 -0500

    Updated ifdefs to match the project

--- include/uThreads.h
+++ include/uThreads.h
@@ -15,12 +15,13 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 
-#ifndef SRC_INCLUDE_UTHREADS_H_
-#define SRC_INCLUDE_UTHREADS_H_
+#ifndef UTHREADS_INCLUDE_UTHREADS_H_
+#define UTHREADS_INCLUDE_UTHREADS_H_
+
 #include "runtime/uThread.h"
 #include "runtime/kThread.h"
 #include "runtime/Cluster.h"
 #include "io/Network.h"
 #include "runtime/BlockingSync.h"
 
-#endif /* SRC_INCLUDE_UTHREADS_H_ */
+#endif /* UTHREADS_INCLUDE_UTHREADS_H_ */
