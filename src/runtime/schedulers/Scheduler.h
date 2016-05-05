4a85e3169f1e75d65416a23f0393b73bbb7e6140
Saman Barghi <saman.b@gmail.com>
2016-05-04 23:31:14 -0400

    Provide a way for changing schedulers
    By passing -DSCHEDULERNO=x where x the scheduler number, it's
    possible to switch between schedulers at compile time.

--- src/runtime/schedulers/Scheduler.h
+++ src/runtime/schedulers/Scheduler.h
@@ -7,7 +7,12 @@
 
 #ifndef SRC_RUNTIME_SCHEDULERS_SCHEDULER_H_
 #define SRC_RUNTIME_SCHEDULERS_SCHEDULER_H_
+
+#if SCHEDULERNO == 1
 #include "Scheduler_01.h"
+#elif SCHEDULERNO == 2
+#include "Scheduler_02.h"
+#endif
 
 
 #endif /* SRC_RUNTIME_SCHEDULERS_SCHEDULER_H_ */
