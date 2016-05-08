ff3eaa80980d2e65ac382914b1b4ea2e4bcdf83b
Saman Barghi <saman.b@gmail.com>
2016-05-08 16:33:32 -0400

    V0.2.0

--- search/all_f.js
+++ search/all_f.js
@@ -2,5 +2,15 @@ var searchData=
 [
   ['uthreads_3a_20concurrent_20user_2dlevel_20threads_20in_20c_2b_2b',['uThreads: Concurrent User-level Threads in C++',['../index.html',1,'']]],
   ['uthread',['uThread',['../classuThread.html',1,'uThread'],['../classuThread.html#a7491d81078b3ded533c600137cde8325',1,'uThread::uThread(vaddr sb, size_t ss)'],['../classuThread.html#ad3352d002db818f58f94093dc3636fe4',1,'uThread::uThread(const uThread &amp;)=delete']]],
+  ['uthread_5fcreate',['uThread_create',['../cwrapper_8h.html#acfd14530be3fea8cf9204f70cd9e4b9e',1,'cwrapper.cpp']]],
+  ['uthread_5fdetach',['uThread_detach',['../cwrapper_8h.html#a5bc04089100a2af87d1b94c5c6a58fdc',1,'cwrapper.cpp']]],
+  ['uthread_5fget_5fcurrent',['uThread_get_current',['../cwrapper_8h.html#ae327f6468bf4aeba31243ef5cd49fcdc',1,'cwrapper.cpp']]],
+  ['uthread_5fget_5fid',['uThread_get_id',['../cwrapper_8h.html#add2aa0519b9655b2a295653f8fab0a01',1,'cwrapper.cpp']]],
+  ['uthread_5fget_5ftotal_5fnumber_5fof_5futhreads',['uThread_get_total_number_of_uThreads',['../cwrapper_8h.html#a0aff17b56e583c76add0913790f472f2',1,'cwrapper.cpp']]],
+  ['uthread_5fjoin',['uThread_join',['../cwrapper_8h.html#a350a033acc8970710b72d31b0ead6d4b',1,'cwrapper.cpp']]],
+  ['uthread_5fmigrate',['uThread_migrate',['../cwrapper_8h.html#ae884ed2490c204350df69b54ebe62c18',1,'cwrapper.cpp']]],
+  ['uthread_5fstart',['uThread_start',['../cwrapper_8h.html#af6fe0d5fb1b8c45f0b6c7f21c12721fb',1,'cwrapper.cpp']]],
+  ['uthread_5fterminate',['uThread_terminate',['../cwrapper_8h.html#a042b0598787dfca933132305f9324e4c',1,'cwrapper.cpp']]],
+  ['uthread_5fyield',['uThread_yield',['../cwrapper_8h.html#a4d8091f1b2362987907c2fba1c77af5c',1,'cwrapper.cpp']]],
   ['uthreadcache',['uThreadCache',['../classuThreadCache.html',1,'']]]
 ];
---
+++ search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cwrapper_2eh',['cwrapper.h',['../cwrapper_8h.html',1,'']]]
+];
