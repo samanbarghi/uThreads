e69d61b75195ea3417514e42dabbc35942169a88
Saman Barghi <saman.b@gmail.com>
2016-05-31 17:47:01 -0400

    Scheduler: define default scheduler in scheduler.h
    Instead of Makefile move the definition of the default scheduler to
    scheduler.h.

--- src/runtime/schedulers/Scheduler.h
+++ src/runtime/schedulers/Scheduler.h
@@ -8,6 +8,10 @@
 #ifndef SRC_RUNTIME_SCHEDULERS_SCHEDULER_H_
 #define SRC_RUNTIME_SCHEDULERS_SCHEDULER_H_
 
+#ifndef SCHEDULERNO
+#define SCHEDULERNO 2
+#endif
+
 #if SCHEDULERNO == 1
 #include "Scheduler_01.h"
 #elif SCHEDULERNO == 2
