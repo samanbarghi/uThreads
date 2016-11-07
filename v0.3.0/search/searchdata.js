93be55b752288a976bd8ee2b2fcac21f666dff48
Saman Barghi <saman.b@gmail.com>
2016-11-07 02:36:36 -0500

    Added http results and acknowledgement

--- v0.3.0/search/searchdata.js
+++ v0.3.0/search/searchdata.js
@@ -1,27 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "abcdegijklmoprstuvwy",
-  1: "bcikmosu",
-  2: "c",
-  3: "abcdegjklmoprstuvwy",
-  4: "u"
+  0: "u",
+  1: "u"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Pages"
+  1: "Pages"
 };
 
